7f7d8162d36d4e80df97564b5d78972b21c531f9
Hector <hmorenoveleiro@gmail.com>
2025-11-13 09:56:17 +0100

    .

---
+++ src/EjerciciosArraysUnidimensionales/Ejercicio3.java
@@ -0,0 +1,9 @@
+package EjerciciosArraysUnidimensionales;
+
+public class Ejercicio3 {
+    /*
+     * Escribe un programa que lea 10 números por teclado y que luego los muestre
+     * en orden inverso, es decir, el primero que se introduce es el último en
+     * mostrarse y viceversa.
+     */
+}

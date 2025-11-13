9e96a2fff1bd2451ddfbeae0c61e7aaa13c64298
Hector <hmorenoveleiro@gmail.com>
2025-11-13 12:35:35 +0100

    .

--- src/tres/ProgresionGeometrica.java
+++ src/tres/ProgresionGeometrica.java
@@ -2,30 +2,33 @@ package tres;
 
 import EjerciciosClase.AjustesProgramas;
 
-public class Propuesto11 {
+public class ProgresionGeometrica {
     // programa que muestre los 50 primeros pares
     public static void main(String[] args) {
+
         int primerNumero = AjustesProgramas.introducirIntegerPorScanner("primer termino --> ");
         int diferenciaASumar = AjustesProgramas.introducirIntegerPorScanner("diferencia a sumar --> ");
         int terminoAObtener = AjustesProgramas.introducirIntegerPorScanner("dime que termino deseas obtener --> ");
+
         int termino = primerNumero;
         int acumula = primerNumero;
+
         for (int i = primerNumero; i < terminoAObtener; i++) {
             System.out.println(termino + " ");
-            termino += diferenciaASumar;
-            acumula += termino;
+            termino *= diferenciaASumar;
+            // acumula += termino;
         }
-        System.out.println("el termino " + terminoAObtener + " es: " + termino);
-        System.out.println("la suma de los " + terminoAObtener + " primeros terminos de la sucesion es " + acumula);
 
-        // sucesion
+        System.out.println("el termino " + terminoAObtener + " es: " + termino);
+        // System.out.println("la suma de los " + terminoAObtener + " primeros terminos
+        // de la sucesion es " + acumula);
+
         long primer = AjustesProgramas.introducirLongPorScanner("introduce el primero --> ");
         long ultimo = AjustesProgramas.introducirLongPorScanner("introduce el ultimo --> ");
 
-        // Usa solo operaciones con 'long' para evitar problemas de Math.pow
         long n = ultimo - primer + 1;
         long suma = (n * (primer + ultimo)) / 2;
-        System.out.println(
-                "la suma de tu sucesion es " + (long) (suma));
+
+        // System.out.println("la suma de tu sucesion es " + (long) (suma));
     }
 }
